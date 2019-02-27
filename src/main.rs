0b94c22925ca5afbe1930c289622bb89bff8fcf1
Noah Rosenzweig <noah@aware.vision>
2019-02-27 16:18:12 +0100

    Respond with 404 Not Found when not requesting '/'

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::fs;
 use std::path::Path;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::collections::HashMap;
-use std::sync::Mutex;
 
 macro_rules! path {
     ( $( $x:expr ),* ) => {
@@ -22,7 +20,6 @@ fn main() {
     let listener = TcpListener::bind(&format!("0.0.0.0:{}", PORT)).unwrap();
 
     for stream in listener.incoming() {
-        println!("stream");
         let stream = stream.unwrap();
         handle_connection(stream);
     }
@@ -32,14 +29,16 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
+    let response;
     let index_get = b"GET / HTTP/1.1\r\n";
 
     if buffer.starts_with(index_get) {
         let contents = fs::read_to_string(path!(HTML_DIR, "index.html")).unwrap();
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+        response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
     } else {
-
+        response = String::from("HTTP/1.1 404 Not Found\r\n\r\n");
     }
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
