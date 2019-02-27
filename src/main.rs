33fe0fbd8decf92dbb548c6b31a0b97af4c06821
Noah Rosenzweig <noah@aware.vision>
2019-02-27 16:15:21 +0100

    Respond with HTML

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use std::io::prelude::*;
+use std::fs;
+use std::path::Path;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::collections::HashMap;
+use std::sync::Mutex;
+
+macro_rules! path {
+    ( $( $x:expr ),* ) => {
+        Path::new(".")
+            $(
+                .join($x.to_string())
+            )*
+    }
+}
 
 const PORT: usize = 7878;
+const HTML_DIR: &str = "html/";
 
 fn main() {
     let listener = TcpListener::bind(&format!("0.0.0.0:{}", PORT)).unwrap();
 
     for stream in listener.incoming() {
+        println!("stream");
         let stream = stream.unwrap();
         handle_connection(stream);
     }
@@ -16,5 +31,15 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let index_get = b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(index_get) {
+        let contents = fs::read_to_string(path!(HTML_DIR, "index.html")).unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+
+    }
 }
